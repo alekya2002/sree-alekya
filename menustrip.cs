6490c86172982ea1798ae2c99f275efe8884e52f
alekya2002 <71516002+alekya2002@users.noreply.github.com>
2020-11-03 15:42:18 +0000

    Add files via upload

---
+++ menustrip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace menustrip
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            UserAdd ua = new UserAdd();
+            ua.ShowDialog();
+        }
+    }
+}
